cefece73b3536ef86c4b15d079ce682c16056592
MBriedis <mbriedis@gmail.com>
2019-07-14 11:21:00 +0300

    Parser reformat, PHPDOC fixes

--- src/Parser.php
+++ src/Parser.php
@@ -2,43 +2,42 @@
 
 namespace Waddle;
 
+use Exception;
+
 abstract class Parser
 {
-    
     abstract public function parse($file);
 
     /**
      * Check if file is available
      * @param string $pathname
-     * @throws \Exception
+     * @throws Exception
      */
-    protected function checkForFile($pathname) {
-        if (!is_file($pathname)){
-            throw new \Exception("Could not load file: {$pathname}");
+    protected function checkForFile($pathname)
+    {
+        if (!is_file($pathname)) {
+            throw new Exception("Could not load file: {$pathname}");
         }
     }
-    
+
     /**
      * Calculate the distance in KM, between two lat/lon points
-     * @param type $fromLat
-     * @param type $toLat
-     * @param type $fromLon
-     * @param type $toLon
-     * @param type $earthRadius
-     * @return type
+     * @param float $fromLat
+     * @param float $toLat
+     * @param float $fromLon
+     * @param float $toLon
+     * @param int $earthRadius
+     * @return float
      */
     protected function calculateDistanceBetweenLatLon($fromLat, $toLat, $fromLon, $toLon, $earthRadius = 6373)
     {
-                
         $latDelta = deg2rad($toLat - $fromLat);
         $lonDelta = deg2rad($toLon - $fromLon);
-        
-        $angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) + cos(deg2rad($fromLat)) * cos(deg2rad($toLat)) * pow(sin($lonDelta / 2), 2)));
+
+        $angle = 2 * asin(sqrt(pow(sin($latDelta / 2),2) + cos(deg2rad($fromLat)) * cos(deg2rad($toLat)) * pow(sin($lonDelta / 2), 2)));
 
         $distance = $angle * $earthRadius;
 
-        return \Waddle\Converter::convertKilometresToMetres($distance);
-        
+        return Converter::convertKilometresToMetres($distance);
     }
-    
 }
